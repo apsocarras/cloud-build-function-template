75bfd130a0086a67f2d872ad865237d2feee2ada
Alexander Socarras <alex@Alexanders-Mac-mini.local>
2025-02-25 18:01:49 -0500

    fix output

--- {{cookiecutter.project_name}}/scripts/gcloud_setup/setup_secret.sh
+++ {{cookiecutter.project_name}}/scripts/gcloud_setup/setup_secret.sh
@@ -7,17 +7,17 @@ SERVICE_ACCOUNT_NAME=$4
 
 ## Check if the secret exists; create if not 
 if gcloud secrets describe "$SECRET_NAME" --project "$PROJECT_ID" &>/dev/null; then 
-    echo "Secret $SECRET_NAME exists in the project $PROJECT_ID."
-    return 1
+    echo "projects/$PROJECT_ID/secrets/$SECRET_NAME/versions/1"
 else 
     echo "Secret $SECRET_NAME does not exist in the project $PROJECT_ID. Creating."
     echo -n "$SECRET_VALUE" | gcloud secrets create "$SECRET_NAME" --data-file=-=
+
+    ## Grant access to the service account 
+    gcloud secrets add-iam-policy-binding "$GCP_PAT_SECRET_NAME" \
+        --member="serviceAccount:${SERVICE_ACCOUNT_NAME}" \
+        --role="roles/secretmanager.secretAccessor"
+    
+    echo "projects/$PROJECT_ID/secrets/$SECRET_NAME/versions/1"
 fi
 
-## Grant access to the service account 
-gcloud secrets add-iam-policy-binding "$GCP_PAT_SECRET_NAME" \
-    --member="serviceAccount:${SERVICE_ACCOUNT_NAME}" \
-    --role="roles/secretmanager.secretAccessor"
 
-
-echo "projects/$PROJECT_ID/secrets/$SECRET_NAME/versions/1"
