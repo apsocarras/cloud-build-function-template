c57563ec595424b5338ea01f22f8018faf6f1555
Alexander Socarras <alex@Alexanders-Mac-mini.local>
2025-02-25 16:16:20 -0500

    WIP: github cli access

--- {{cookiecutter.project_name}}/scripts/setup.sh
+++ {{cookiecutter.project_name}}/scripts/setup.sh
@@ -5,6 +5,7 @@
 PROJECT_NAME="{{cookiecutter.project_name}}" # gh repo name
 GITHUB_PAT="{{cookiecutter.github_pat}}"
 GITHUB_AUTHOR="{{cookiecutter.author_github_handle}}"
+GITHUB_CLOUD_BUILD_INSTALLATION_ID="{{cookiecutter.github_cloud_build_app_installation_id}}"
 GCP_PROJECT_ID="{{cookiecutter.gcp_project_id}}" # name of organizing project in GCP
 GCP_REGION_ID="{{cookiecutter.gcp_region_id}}"
 GCP_ARTIFACT_REGISTRY_REPO="{{cookiecutter.gcp_artifact_registry_repo_name}}"
@@ -23,18 +24,23 @@ DEFAULT_CLOUD_BUILD_SERVICE_AGENT="service-${GCP_PROJECT_NUMBER}@gcp-sa-cloudbui
 
 ### Authenticate shell session with: gcloud auth login and gh cli
 ## gh auth login 
-if gh auth status --json user --quiet > /dev/null; then
-    echo "Authenticated with GitHub CLI"
-else
+
+GH_ACTIVE_ACCOUNT=$(gh auth status --active | sed -n 's/.*github.com account \([^ ]*\).*/\1/p')
+if [[ "$GH_ACTIVE_ACCOUNT" == "$GITHUB_AUTHOR" ]]; then 
+    echo "Authenticated with GitHub CLI as ${GITHUB_AUTHOR}"
+elif [[ "$GH_ACTIVE_ACCOUNT" != "" ]]; then
+    echo "Warning: logged into GitHub CLI with $GH_ACTIVE_ACCOUNT (!= $GITHUB_AUTHOR)"
+    return 1
+else 
     echo "Not authenticated with GitHub CLI"
-    exit 1
+    return 1
 fi
 ## gcloud auth login
 if gcloud auth list --filter=status:ACTIVE --format="value(account)" > /dev/null; then
     echo "Authenticated with gcloud CLI"
 else
     echo "Not authenticated with gcloud CLI"
-    exit 1
+    return 1
 fi
 
 ## Verify that the core.project in gcloud CLI is the same as the cookiecutter setting 
@@ -42,18 +48,21 @@ CUR_PROJECT_ID=$(gcloud config list --format="value(core.project)")
 if [[ "$CUR_PROJECT_ID" != "$GCP_PROJECT_ID" ]]; then 
     echo "Current value(core.project) ($CUR_PROJECT_ID) is different from cookiecutter input ($GCP_PROJECT_ID)."
     echo "Run 'gcloud config set project $GCP_PROJECT_ID' and rerun the script".
-    exit 1
+    return
 fi 
 
 ### Create github repository: {{cookiecutter.author_github_handle}}/{{cookiecutter.project_name}}
-export REPO_ENDPOINT="${GITHUB_AUTHOR}/${PROJECT_NAME}"
-response_message=$(curl -s -w "%{message}" -o response.txt https://api.github.com/repos/${REPO_ENDPOINT})
+URL="https://api.github.com/repos/${GITHUB_AUTHOR}/${PROJECT_NAME}"
+response=$(curl -s -w -o response.txt \
+    -H "Authorization: token ${GITHUB_PAT}" \
+    https://api.github.com/repos/${GITHUB_AUTHOR}/${PROJECT_NAME})
 
-if [[ "$response_message" == "Not Found"]]; then 
-    echo "Found github repo at: $REPO_ENDPOINT"
+if [[ "$response" == "Not Found" ]]; then 
+    echo "Found github repo at: ${URL}"
 else 
-    echo "GitHub repo not found: $REPO_ENDPOINT"
-    exit 1
+    echo "WARNING: GitHub repo not found (${URL})."
+    echo "INFO: Note that your current GitHub account might have permissions to upload to the repository, but not list it programmatically."
+    echo "INFO: This can happen if the repo is a private repo in an organization you're a member of."
 fi 
 
 #### -------------------- ####
@@ -77,7 +86,9 @@ gcloud secrets add-iam-policy-binding $GCP_PAT_SECRET_NAME \
 GCP_GH_CONNECTION_NAME="${PROJECT_NAME}-gh-connection"
 gcloud builds connections create github $GCP_GH_CONNECTION_NAME \
     --authorizer-token-secret-version=projects/$GCP_PROJECT_ID/secrets/$GCP_PAT_SECRET_NAME/versions/1 \
-    --app-installation-id=
+    --app-installation-id=$GITHUB_CLOUD_BUILD_INSTALLATION_ID \
+    --region=$GCP_REGION_ID
+
 
 # Connect the repo via the Connection
 GITHUB_URI="https://github.com/{$GITHUB_AUTHOR}/{$PROJECT_NAME}.git"
@@ -86,7 +97,7 @@ gcloud builds repositories create $PROJECT_NAME \
     --connection=$GCP_GH_CONNECTION_NAME --region=$GCP_REGION_ID
 
 ## CREATE ARTIFACT REPOSITORY
-gcloud artifacts repository create ${GCP_ARTIFACT_REGISTRY_REPO} \
+gcloud artifacts repositories create ${GCP_ARTIFACT_REGISTRY_REPO} \
     --repository-format=docker \
     --location=$GCP_REGION_ID \
     --description="Repository for images related to the ${PROJECT_NAME} cloud build project"
